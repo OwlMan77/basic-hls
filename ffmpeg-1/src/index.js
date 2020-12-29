62fb0f8609b8e1034e6db2164967174e199b96bf
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2020-12-29 01:38:20 +0000

    Created basic ffmpeg converter for mp4.

---
+++ ffmpeg-1/src/index.js
@@ -0,0 +1,11 @@
+const ffmpeg = require('fluent-ffmpeg');
+
+ffmpeg(`${__dirname}/input/Free_Apple.mp4`, { timeout: 432000 }).addOptions([
+    '-profile:v baseline', // baseline profile (level 3.0) for H264 video codec
+    '-level 3.0', 
+    '-s 640x360',          // 640px width, 360px height output video dimensions
+    '-start_number 0',     // start the first .ts segment at index 0
+    '-hls_time 10',        // 10 second segment duration
+    '-hls_list_size 12',    // Maxmimum number of playlist entries (0 means all entries/infinite)
+    '-f hls'               // HLS format
+  ]).output(`${__dirname}/output/free_apple.m3u8`).on('end', (data) => console.log(data)).run()
