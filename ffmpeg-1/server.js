b2b2c65141077783c2fa74139ad2c2205301054c
Jean-Marc McGhee <jeanmarc.mcghee@gmail.com>
2020-12-30 02:40:56 +0000

    Made a basic html page with hls.js to demo. (Was basic change from the hls example.)

--- ffmpeg-1/server.js
+++ ffmpeg-1/server.js
@@ -1,12 +1,11 @@
-// provide an endpoint that will 
-
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 
 const outputHLS = express.static(path.join(__dirname, 'src', 'output'))
 
+app.use(cors())
 app.use('/static', outputHLS)
 
 app.listen(3000, () => {
